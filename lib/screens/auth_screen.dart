26a173b2ddac43967367217878ceb3a1d07ddd1b
Nopnapat Norasri <neuw@Nopnapats-MacBook-Pro.local>
2023-09-28 11:57:25 +0700

    fix(screens): auth -> userCredentials

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -42,7 +42,7 @@ class _AuthScreenState extends State<AuthScreen> {
         _isAuthenticating = true;
       });
       if (_isLogin) {
-        await _firebase.signInWithEmailAndPassword(
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
       } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
